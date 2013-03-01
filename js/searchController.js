afce52e333d67e972c729d0b486aeee06232d2e6
Terry Smith <terry@shebiki.org>
2013-03-01 12:41:14 -0500

    Trims down the project and updates it to Simplicity 4.0. Pending a squash down to a single commit.

---
+++ js/searchController.js
@@ -0,0 +1,37 @@
+(function ($, window) {
+    window.search_controller = function (state) {
+        var pageSize = 10;
+        var request = {
+            criteria: [],
+            properties: [],
+            facets: {
+                genre: {
+                  depth: 0,
+                  topN: 15,
+                  sortBy: 'countDesc'
+                }
+            },
+            startIndex: state.page ? (pageSize * (state.page - 1)) : 0,
+            pageSize: pageSize
+        };
+        if (state.q) {
+            request.criteria.push({
+                dimension: 'freetext',
+                value: state.q,
+                cull: true
+            });
+        }
+        if (state.genre) {
+            request.criteria.push({
+                dimension: 'genre',
+                id: state.genre
+            });
+        }
+        if (request.criteria.length ===  0) {
+            // If no search criteria were provided, we can
+            // configure a default search here.
+            request.criteria.push({dimension: '_kind'});
+        }
+        return request;
+    };
+})($, window);
--- js/searchResults.js
+++ js/searchResults.js
@@ -1,6 +1,6 @@
 (function ($, window) {
     window.search_results = function (target, data) {
-        var results = $('<div class="results"><div class="summary">There are <span class="exact"/> exact matches out of <span class="total"/> results</div></div>');
+        var results = $('<div><div class="summary">There are <span class="exact"/> exact matches out of <span class="total"/> results</div></div>');
         var discoveryResponse = data._discovery.response;
         results.find(".exact").text(discoveryResponse.exactSize);
         results.find(".total").text(discoveryResponse.totalSize);
@@ -18,6 +18,6 @@
             result.find('pre').text(JSON.stringify(item, null, '    '));
             results.append(result);
         });
-        target.html(results);
+        target.html(results.contents());
     };
 })($, window);
